2184ab4bf2a65cdbdf4547fbe64b553cd04d5bf8
Nephilim <nephilim@Belcebux99.eie.ucr.ac.cr>
2025-04-02 21:18:17 -0600

    Sequence class done

--- UVM/Combinational_adder/Combinational_MODULE_TB.sv
+++ UVM/Combinational_adder/Combinational_MODULE_TB.sv
@@ -17,10 +17,44 @@ class transaction extends uvm_sequence_item;
 		super.new(path;
 	endfunction
 
-/// Macros for print, copying, comparing
+/// Macros for field automation: print, copying, comparing
 `uvm_objects_utils_begin(transaction)
 `uvm_field_int(a, UVM_DEFAULT)
 `uvm_field_int(b, UVM_DEFAULT)
 `uvm_field_int(y, UVM_DEFAULT)
 `uvm_object_utils_end
 endclass
+///________________________________________________________________________
+/////_____Sequence_____/////
+/// Combination of transactions to verify specific testcases (uvm_sequence)
+/// "#(transaction)": parameterized class that works with "transaction"
+class generator extends uvm_sequence #(transaction);
+	/// Register a simple object with no field automation
+	`uvm_object_utils(generator)
+
+	///// Instance transaction class
+	/// Used because of start and finish item
+	transaction t;
+	/// Counter for stimuli
+	integer i;
+	/// Constructor
+	function new(input string path = "generator");
+		super.new(path);
+	endfunction
+	/// Main execution block: Its virtual due to polymorphism
+	/// Child classes can redifine body without modifying base class
+	virtual task body();
+		t = transaction::type_id::create("t");
+		repeat(10)
+			begin
+				/// Request access the driver
+				start_item(t);
+				/// Random number generator
+				t.randomize();
+				/// UVM reporting macros
+				`uvm_info("GEN", $sformatf("Data send to driver a: %0d, b: %0d", t.a,t.b),UVM_NONE);
+				/// Send to the driver
+				finish_item(t); 
+	endtask
+endclass
+///________________________________________________________________________	
